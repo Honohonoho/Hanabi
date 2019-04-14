180d59896d68bf370867d221cb18750c491915ef
Honohonoho <751718620@qq.com>
2019-04-14 15:54:58 +0800

    修改 icon 组件，添加 helpper: importAllIcons.js 非静态引入全部svg功能

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import wechat from './icons/wechat.svg';
-
-console.log(wechat);
+// import './icons/wechat.svg';
+// import './icons/alipay.svg';
+// import './icons/qq.svg';
+import './importAllIcons';
 
 interface IconProps {
     name: String
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
+    // @ts-ignore
     return (
-        <span>{props.name}</span>
+        <span>
+            <svg>
+                <use xinkHref={`#${props.name}`}/>
+            </svg>
+        </span>
     );
 };
 
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
-ReactDOM.render(<div><Icon name="wechat"/></div>, document.body);
+ReactDOM.render(
+    <div>
+        <Icon name="wechat"/>
+        <Icon name="alipay"/>
+        <Icon name="qq"/>
+    </div>
+    , document.getElementById('app')
+);
