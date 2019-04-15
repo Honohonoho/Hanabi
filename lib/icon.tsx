f34d5c6b80ac0718d87357e9dfbc3519fce5977f
Honohonoho <751718620@qq.com>
2019-04-15 22:08:17 +0800

    添加 icon 组件的点击事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -3,12 +3,13 @@ import './importAllIcons';
 import './icons.scss';
 
 interface IconProps {
-    name: String
+    name: String,
+    onClick: (e: React.MouseEvent) => void
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <svg className="h-icon">
+        <svg className="h-icon" onClick={props.onClick}>
             <use xlinkHref={`#${props.name}`}/>
         </svg>
     );
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
+const fn = (e: React.MouseEvent<SVGElement | SVGUseElement>) => {
+    console.log(e);
+    console.log(e.target);
+    console.log((e.target as SVGUseElement).href);
+};
 ReactDOM.render(
     <div>
-        <Icon name="wechat"/>
-        <Icon name="alipay"/>
-        <Icon name="qq"/>
+        <Icon name="wechat" onClick={fn}/>
+        {/*<Icon name="alipay"/>*/}
+        {/*<Icon name="qq"/>*/}
     </div>
     , document.getElementById('app')
 );
