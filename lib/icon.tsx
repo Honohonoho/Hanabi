25bc4711214f12671d75966a9b4c675a456f2c24
Honohonoho <751718620@qq.com>
2019-04-16 21:49:22 +0800

    优化 icon 组件合并多个class逻辑

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -6,10 +6,14 @@ interface IconProps extends React.SVGAttributes<SVGElement> {
     name: string
 }
 
+function classes(...names: (string | undefined)[]) {
+    return names.filter(Boolean).join(' ');
+}
+
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     const {className, ...restProps} = props;
     return (
-        <svg className={`h-icon ${className}`}
+        <svg className={classes('h-icon', className)}
              {...restProps}
         >
             <use xlinkHref={`#${props.name}`}/>
