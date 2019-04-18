ec4fb9c94441896450575f798b4bba51cfdacb1b
Honohonoho <751718620@qq.com>
2019-04-18 20:58:52 +0800

    继续添加 icon 组件单元测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,4 +1,5 @@
 import * as renderer from "react-test-renderer";
+import {mount} from 'enzyme';
 import React from "react";
 import Icon from "../icon";
 
@@ -6,5 +7,11 @@ describe("icon", () => {
     it("icon render successfully", function () {
         const json = renderer.create(<Icon name="wechat"/>).toJSON();
         expect(json).toMatchSnapshot()
+    });
+    it('icon on click', () => {
+        const fn = jest.fn();
+        const component = mount(<Icon name="wechat" onClick={fn}/>);
+        component.find('svg').simulate('click');
+        expect(fn).toBeCalled()
     });
 });
