f8f54d306b8cb550f2106acb249a9fa65440a401
Honohonoho <751718620@qq.com>
2019-04-21 22:51:07 +0800

    调整 Button example

--- lib/button.example.tsx
+++ lib/button.example.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Button from './button';
+
 const ButtonExample: React.FunctionComponent = () => {
     return (
-        <div>I'm button</div>
+        <Button></Button>
     );
 };
 export default ButtonExample;
