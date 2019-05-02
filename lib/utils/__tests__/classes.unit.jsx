ae921e5ca99d5c11a50062a29a2c017544bc4539
Honohonoho <751718620@qq.com>
2019-05-02 21:57:47 +0800

    添加 classesHelper 相关测试用例

--- lib/utils/__tests__/classes.unit.jsx
+++ lib/utils/__tests__/classes.unit.jsx
@@ -1,20 +1,31 @@
-import classesHelper from "../classesHelper";
+import classes, {classNamePrefix} from '../classesHelper';
 
 describe('classes', () => {
     it('accept one class', function () {
-        const result = classesHelper('any-icon');
+        const result = classes('any-icon')
         expect(result).toEqual('any-icon')
     });
     it('accept classes', function () {
-        const result = classesHelper('any-icon-1', 'any-icon-2');
+        const result = classes('any-icon-1', 'any-icon-2');
         expect(result).toEqual('any-icon-1 any-icon-2')
     });
     it('accept classes include undefined', function () {
-        const result = classesHelper('any-icon-1', undefined, 'any-icon-2');
+        const result = classes('any-icon-1', undefined, 'any-icon-2');
         expect(result).toEqual('any-icon-1 any-icon-2')
     });
     it('accept null class', function () {
-        const result = classesHelper();
+        const result = classes();
         expect(result).toEqual('')
     });
 });
+
+describe('classNamePrefix', () => {
+    it('multi classes', () => {
+        const scopeClass = classNamePrefix('h-layout');
+        expect(scopeClass('')).toEqual('h-layout');
+        expect(scopeClass({'header': true, 'active': false})).toEqual('h-layout-header');
+        expect(scopeClass({'header': true, 'active': true})).toEqual('h-layout-header h-layout-active');
+        expect(scopeClass({'header': true, 'active': true}, {extra: 'active'}))
+            .toEqual('h-layout-header h-layout-active active');
+    })
+})
