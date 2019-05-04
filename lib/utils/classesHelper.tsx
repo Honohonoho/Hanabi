90efc7b0ca8d674cda981187c64d0cbe85f41f6d
Honohonoho <751718620@qq.com>
2019-05-04 15:44:40 +0800

    重构 classesHelper

--- lib/utils/classesHelper.tsx
+++ lib/utils/classesHelper.tsx
@@ -15,30 +15,18 @@ interface classToggles {
 
 function classNamePrefix(prefix: string) {
 
-    return function concatClassName(name: string | classToggles, options?: extraClass) {
-
-        const namesAsObject = (typeof name === 'string' || name === undefined) ?
-            {[name]: name} :
-            name;
-
-        const scopeClass = Object
-            .entries(namesAsObject)
+    return (name: string | classToggles, options?: extraClass) =>
+        Object
+            .entries(name instanceof Object ? name : {[name]: name})
             .filter(kv => kv[1] !== false)
             .map(kv => kv[0])
             .map(name => {
                 return [prefix, name]
                     .filter(Boolean)
-                    .join('-')
+                    .join('-');
             })
+            .concat(options && options.extra || [])
             .join(' ');
-
-        if (options && options.extra) {
-            return [scopeClass, options.extra].filter(Boolean).join(' ');
-        } else {
-            return scopeClass;
-        }
-    };
-
 }
 
 export {classNamePrefix};
